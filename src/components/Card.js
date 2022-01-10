0f0f84bd58f784c6dd61def2f28a810eb1ce0ab5
강준희 <junheekang@jeongsohyeon-ui-MacBook-Pro.local>
2022-01-10 18:06:24 +0900

    style: Card css

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,16 +7,20 @@ function Card({ movies }) {
   return (
     <Container>
       {movies.map((movie, index) => (
-        <div key={index}>
-          <h2>{movie.title}</h2>
-          <img src={movie.medium_cover_image} alt={movie.title} />
-          <p>{movie.summary}</p>
-          <ul>
-            {movie.genres.map((genre) => (
-              <li>{genre}</li>
-            ))}
-          </ul>
-        </div>
+        <Box key={index}>
+          <Left>
+            <img src={movie.medium_cover_image} alt={movie.title} />
+          </Left>
+          <Right>
+            <Title>{movie.title}</Title>
+            <Summary>{movie.summary}</Summary>
+            <ul>
+              {movie.genres.map((genre) => (
+                <li>{genre}</li>
+              ))}
+            </ul>
+          </Right>
+        </Box>
       ))}
     </Container>
   );
@@ -24,6 +28,41 @@ function Card({ movies }) {
 
 const Container = styled.div`
   display: flex;
+  flex-direction: column;
+`;
+
+const Box = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 450px;
+  margin: 10px 0px;
+`;
+
+const Left = styled.div`
+  padding-right: 20px;
+  img {
+    height: 200px;
+  }
+`;
+
+const Right = styled.div``;
+
+const Title = styled.h1`
+  font-size: 25px;
+  font-weight: 600;
+  padding-bottom: 15px;
+`;
+
+const Summary = styled.div`
+  height: 50px;
+  display: -webkit-box;
+  font-size: 16px;
+  white-space: normal;
+  -webkit-line-clamp: 3;
+  -webkit-box-orient: vertical;
+  overflow: hidden;
+  margin-bottom: 10px;
+  /* text-overflow: ellipsis; */
 `;
 
 export default Card;
