39852f79e763bcee303b158e8886e67b5da711e8
강준희 <junheekang@jeongsohyeon-ui-MacBook-Pro.local>
2022-01-10 16:31:17 +0900

    Add: App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import { React, useState, useEffect } from 'react';
+import styled from 'styled-components';
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    fetch('https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year')
+      .then((response) => response.json())
+      .then((json) => {
+        setMovies(json.data.movies);
+        setLoading(false);
+      });
+  }, []);
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <Container>
+      <div>{loading ? <h1>Loading...</h1> : null}</div>
+    </Container>
   );
 }
 
+const Container = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
 export default App;
